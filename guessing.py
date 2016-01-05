1364edd254c452f0423a46d4bcbc3bbb08424f02
Hackbright Student <info@hackbrightacademy.com>
2016-01-05 13:02:39 -0800

    Guessing the Number

--- guessing.py
+++ guessing.py
@@ -1,3 +1,12 @@
 print "Hello! Welcome to the Guessing Game!"
 name = raw_input("What is your name?: ")
 print "Hello {}".format(name)
+number = random.randrange(1, 101)
+
+guess = raw_input("Pick a number between 1 and 100: ")
+
+while guess != number:
+    if guess < number:
+        print "Guess is too low"
+    else print "Guess is too high"
+
