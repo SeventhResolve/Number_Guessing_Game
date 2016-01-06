6ea0613d8ef714b65bcf66d5ad4e48457ca19be2
Hackbright Student <info@hackbrightacademy.com>
2016-01-05 17:38:32 -0800

    Play new game?

--- guessing.py
+++ guessing.py
@@ -2,22 +2,34 @@ print "Hello! Welcome to the Guessing Game!"
 name = raw_input("What is your name?: ")
 print "Hello {}".format(name)
 import random
-number = random.randrange(1, 101)
 
-guess = 0
+def guessing_game():
+    number = random.randrange(1, 101)
 
-while guess != number:
-    try:
-        guess = int(raw_input("Pick a number between 1 and 100: "))
-        if guess > 0 and guess < 101:
-            if guess < number:
-                print "Guess is too low"
-            elif guess > number:
-                print "Guess is too high"
+    guess = 0
+
+    while guess != number:
+        try:
+            guess = int(raw_input("Pick a number between 1 and 100: "))
+            if guess > 0 and guess < 101:
+                if guess < number:
+                    print "Guess is too low"
+                elif guess > number:
+                    print "Guess is too high"
+                else:
+                    print "Congrats!"
+                    restart = raw_input("Would you like to play a new game? Y or N: ")
+                    if restart.lower() == "n":
+                        break
+                    elif restart.lower() == "y":
+                        return guessing_game()
+                    else:
+                        print "Invalid input."
+                        break
             else:
-                print "Congrats!"
-        else:
-            print "Guess is out of range. Try again."
-    except ValueError:
-        print "Oops! That was not a valid number. Try again."
-    
+                print "Guess is out of range. Try again."
+        except ValueError:
+            print "Oops! That was not a valid number. Try again."
+
+
+guessing_game()
