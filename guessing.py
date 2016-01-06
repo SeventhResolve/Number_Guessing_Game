e10be6196a234af3dec8d322a1e43ce146e1cec4
Hackbright Student <info@hackbrightacademy.com>
2016-01-05 16:22:42 -0800

    Check if int

--- guessing.py
+++ guessing.py
@@ -7,10 +7,14 @@ number = random.randrange(1, 101)
 guess = 0
 
 while guess != number:
-    guess = int(raw_input("Pick a number between 1 and 100: "))
-    if guess < number:
-        print "Guess is too low"
-    elif guess > number:
-        print "Guess is too high"
-    else:
-        print "Congrats!"
+    try:
+        guess = int(raw_input("Pick a number between 1 and 100: "))
+        if guess < number:
+            print "Guess is too low"
+        elif guess > number:
+            print "Guess is too high"
+        else:
+            print "Congrats!"
+    except ValueError:
+        print "Oops! That was not a valid number. Try again."
+    
